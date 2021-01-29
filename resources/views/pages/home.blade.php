cb502c34d07ff8f7d4b3852193f2da74305d1149
Pierre-Elie FORTIN <pierre-elie.fortin@Digital-Grenoble.local>
2021-01-29 12:56:43 +0100

    etape 1 itération 2

--- resources/views/pages/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('base')
-
-@section('content')
-<h1>&hearts; pef from France &hearts;</h1>
-
-
-<p> il est {{date('H:i a')}}</p>
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages/home');
+    return 'home page';
+    //return view('pages/homepage');
+
 });
 
-Route::get('/about-us', function () {
-    return view('pages/about');
+Route::get('/product', function () {
+    return 'liste des produits';
+   // return view('pages/listProduct');
+});
+
+Route::get('/product/{id}', function ($id) {
+    return 'Fiche du produit :' .$id;
+    //return view('pages/detailsproduct');
+});
+
+Route::get('/cart', function () {
+    return 'Panier';
+   // return view('pages/basket');
 });
