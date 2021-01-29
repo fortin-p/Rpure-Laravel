a12ebc4f0518d0ed35c10697d6001548b86d76a0
Pierre-Elie FORTIN <pierre-elie.fortin@Digital-Grenoble.local>
2021-01-29 11:09:46 +0100

    Ajout des fichiers

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('base')
+
+@section('content')
+
+
+<p> il est {{date('H:i a')}}</p>
+<p><a href="/">Page d'accueil</a></p>
+
+@endsection
+
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('base')
+
+@section('content')
+<h1>&hearts; pef from France &hearts;</h1>
+
+
+<p> il est {{date('H:i a')}}</p>
+
+@endsection
