2ce70f3e9f3113c333c16f2e298742fab8a8ea1f
fortin-p <fortin.p@hotmail.fr>
2021-02-02 14:40:45 +0100

    modification des fichiers

--- resources/views/voyage.blade.php
+++ resources/views/voyage.blade.php
@@ -1,3 +1,19 @@
+@section('titre')
+    Voyage, voyage
+@endsection
+@section('photoCanada')
+    <img src="{{ asset('images/canada.jpg') }}" alt="Voyage banner" class="photo">
+@endsection
+@section('iconePanier')
+<a href=""> <img src="{{ asset('images/Panier.png') }}" alt="panier" class="panier"> </a>
+@endsection
+@section('detailsButton')
+    <button class="detailsbutton"
+            type="button">
+        Details
+    </button>
+@endsection
+
 @include('display_voyage')
 
 
