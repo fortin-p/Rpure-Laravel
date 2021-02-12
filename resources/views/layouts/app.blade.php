9b6a708807edc9ede185526c77f94fcf632cd7db
Pierre-Elie FORTIN <pierre-elie.fortin@Digital-Grenoble.local>
2021-02-12 13:18:16 +0100

    wip autentification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
--- routes/web.php
+++ routes/web.php
@@ -22,31 +22,3 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
-
-Route::get('/',[HomeController::class,'index']);
-
-Route::get('/product',[ProductController::class,'listProduct']);
-Route::get('/product/{id}',[ProductController::class,'product']);
-
-Route::get('/cart/{id}',[CartController::class,'show']);
-route::post('cart',[CartController::class,'store']);
-
-Route::get('/voyage',[ProductController::class,'show']);
-Route::get('/voyage/{id}',[ProductController::class,'store']);
-
-route::get('/voyagename',[ProductController::class,'getData']);
-route::get('/voyageprice',[ProductController::class,'getDataByPrice']);
-route::get('/voyagenameandprice',[ProductController::class,'getDataByNameAndPrice']);
-
-route::get('/backoffice',[backofficecontroller::class,'index']);
-route::get('/backoffice/create',[backofficecontroller::class,'create']);
-route::post('/backoffice_update',[backofficecontroller::class,'store']);
-
-route::get('/backoffice/{id}/edit',[backofficecontroller::class,'edit']);
-route::put('/backoffice/{id}/edit',[backofficecontroller::class,'update']);
-route::delete('/delete/{name}', [backofficecontroller::class,'delete']);
-
-
-route::get('/categorie/product/',[CategoriesController::class,'show'])->name('products.category');
-
-route::get('/customer',[CustomerController::class,'show']);
